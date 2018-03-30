49e77c571f3e6a7e3e160e4197a981cf77cca7e9
ipaulygon <paulandrei@ymail.com>
2018-03-30 21:36:05 +0800

    change image

--- resources/views/product/active.blade.php
+++ resources/views/product/active.blade.php
@@ -15,7 +15,13 @@
         @foreach($products as $product)
         <tr>
             <td id="dataImage" data-image="{{$product->image}}">
-                <img class="img-responsive" src="{{URL::asset('https://s3.amazonaws.com/us-periodjewels/'.$product->image->first()->image)}}" alt="" style="max-width:150px; background-size: contain">
+            @if(!empty($product->image))
+                @if(!empty($product->image->where('isMain',1)->get()))
+                <img class="img-responsive" src="{{URL::asset('https://s3.amazonaws.com/us-periodjewels/'.$product->image->where('isMain',1)->first()->image)}}" alt="" style="max-width:150px; background-size: contain">                            
+                @else
+                <img class="img-responsive" src="{{URL::asset('https://s3.amazonaws.com/us-periodjewels/'.$product->image->first()->image)}}" alt="" style="max-width:150px; background-size: contain">                            
+                @endif
+            @endif
             </td>
             <td id="dataGem" data-gem="{{$product->gem->id}}">{{$product->gem->name}}</td>
             <td id="dataCarat" data-carat="{{$product->carat}}">{{number_format($product->carat,2)}}</td>
