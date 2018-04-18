440668974709c5f4595a98087c9471a9da60c3bd
ipaulygon <paulandrei@ymail.com>
2018-04-18 21:53:18 +0800

    database

--- config/database.php
+++ config/database.php
@@ -46,16 +46,19 @@ return [
             'prefix' => '',
         ],
 
-        'mysql'  => [
-            'driver'    => 'mysql',
-            'host'      => env('DB_HOST', 'us-cdbr-iron-east-05.cleardb.net'),
-            'database'  => env('DB_DATABASE', 'heroku_0d28cd689873aae'),
-            'username'  => env('DB_USERNAME', 'bbe2484547f75e'),
-            'password'  => env('DB_PASSWORD', 'ed18f063'),
-            'charset'   => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix'    => '',
-            'strict'    => false,
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
         ],
 
         'pgsql' => [
